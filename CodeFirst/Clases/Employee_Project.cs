48dae3e743b3add4a4bf29c2b6331c38445d974b
Paulino Vazquez <poul846@gmail.com>
2019-03-21 17:49:45 -0600

    Add files via upload

---
+++ CodeFirst/Clases/Employee_Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Clases
+{
+    class Employee_Project
+    {
+
+        public int ID_ { get; set; }
+        public string Project_ID { get; set; }
+        public string employee_ID { get; set; }
+        public string Role_ID { get; set; }
+
+
+    }
+}
---
+++ CodeFirst/ClientContest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class ClientContest
+    {
+        public DbSet<Cat_Role> Cat_Roles { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Company> Companys { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
